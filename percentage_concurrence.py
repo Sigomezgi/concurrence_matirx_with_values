b17437dd7c008ecf6c5cd02a114a7849351f3cf1
Sigomezgi <94578395+Sigomezgi@users.noreply.github.com>
2023-03-13 10:54:48 -0500

    Update percentage_concurrence.py

--- percentage_concurrence.py
+++ percentage_concurrence.py
@@ -1,1 +1,15 @@
-df.corr(method=lambda x, y: len (set(x).intersection(set(y)))/len(set(x)))
+#The elements must be uniques
+# Example like what is df.
+
+####
+# dict_clients = {}
+# fechas= list (abogados["fecha"].unique())
+
+# for i in fechas:
+#     dict_clients[i] = list (abogados[abogados["fecha"]== i]["Nit"].unique())# IMPORTANTE TO UNIQUE
+
+# df = pd.DataFrame(dict([(key, pd.Series(value)) for key, value in dict_clients.items()]))
+
+
+
+df.corr(method=lambda x, y: len (set(x).intersection(set(y)))/len(set(y)))
